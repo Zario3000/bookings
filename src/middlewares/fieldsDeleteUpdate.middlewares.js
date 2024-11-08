ebd028100a8c325e17bc5fc1125a87f30ae63779
Zario3000 <nazarioortizcontreras51@gmail.com>
2024-11-08 17:26:08 -0500

    first commit

---
+++ src/middlewares/fieldsDeleteUpdate.middlewares.js
@@ -0,0 +1,15 @@
+const fieldsDeleteUpdate = (req, res, next) => {
+
+  
+  const fildsDelete = ['email', 'password']
+
+  fildsDelete.forEach((field) => {
+    return delete req.body[field]
+  })
+
+
+  next()
+
+}
+
+module.exports = fieldsDeleteUpdate
---
+++ src/middlewares/password.middlewares.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcrypt')
+
+
+const passwordMiddlewares = async (req, res, next) => {
+  const { password } = req.body
+  const passwordHash = await bcrypt.hash(password, 10)
+  req.passwordHash = passwordHash
+  
+  next()
+}
+
+module.exports = {
+  passwordMiddlewares
+}
